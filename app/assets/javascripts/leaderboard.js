d64bb04ad0642aac80eb8350d80d1ef968279b44
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-29 22:45:42 -0500

    loss function working

--- app/assets/javascripts/leaderboard.js
+++ app/assets/javascripts/leaderboard.js
@@ -13,15 +13,14 @@ function setLeaderboard(){
 			for (i=0; i<10; i++){
 				leaders << response[1];
 			}
-			// $('#leader-one').text('Name: ' + response[0].name + ' Wins: ' + response[0].wins)
 			$('#leader-two').text(response[1].name + ' Wins: ' + response[1].wins)
 			$('#leader-three').text(response[2].name + ' Wins: ' + response[2].wins)
 			$('#leader-four').text(response[3].name + ' Wins: ' + response[3].wins)
 			$('#leader-five').text(response[4].name + ' Wins: ' + response[4].wins)
 			$('#leader-six').text(response[5].name + '  Wins: ' + response[5].wins)
 			$('#leader-seven').text(response[6].name + ' Wins: ' + response[6].wins)
-			$('#leader-eight').text('Name: ' + response[7].name + 'Wins: ' + response[7].wins),
-			// $('#leader-nine').text('Name: ' + response[8].name + 'Wins: ' + response[8].wins),
+			$('#leader-eight').text(response[7].name + ' Wins: ' + response[7].wins)
+			$('#leader-nine').text(response[8].name + ' Wins: ' + response[8].wins)
 			// $('#leader-ten').text('Name: ' + response[9].name + 'Wins: ' + response[9].wins)
 		}, 
 		error: function(){
