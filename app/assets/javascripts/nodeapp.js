f1ae3586b18469023ad320a07a993a8e756c9a18
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-29 23:17:51 -0500

    fixing node app stream

--- app/assets/javascripts/nodeapp.js
+++ app/assets/javascripts/nodeapp.js
@@ -21,6 +21,7 @@ $('#start-button').on('click', function(){
 
     var socket = io.connect('https://twitter-burst-node.herokuapp.com/', { query: query, 'forceNew':true });
     socket.on('stream', function(tweet){
+        console.log(tweet);
         setTimeout(function(){
         if (tweet.search(first_hashtag) != -1){
             $('#first-hashtag-result').text(tweet);
@@ -33,13 +34,13 @@ $('#start-button').on('click', function(){
             numtwo ++;
             $('#ticker-two').text(numtwo)}
         }, 0);
-        setTimeout(function(){
-        if (tweet.search(second_hashtag) != -1) {
-            $('#second-hashtag-result').text(tweet);
-            numtwo = $('#ticker-two').text();
-            numtwo ++;
-            $('#ticker-two').text(numtwo);
-        }}, 0);
+        // setTimeout(function(){
+        // if (tweet.search(second_hashtag) != -1) {
+        //     $('#second-hashtag-result').text(tweet);
+        //     numtwo = $('#ticker-two').text();
+        //     numtwo ++;
+        //     $('#ticker-two').text(numtwo);
+        // }}, 0);
     });
     setTimeout(function () {
       socket.disconnect();
