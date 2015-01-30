38ebd3fe6788426cf27cae58ad64ab95cf38759e
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-29 23:48:08 -0500

    trying out

--- app/assets/javascripts/nodeapp.js
+++ app/assets/javascripts/nodeapp.js
@@ -11,33 +11,41 @@ var numtwo = 0;
     };
 
 $('#start-button').on('click', function(){
-    resetFields();
     var first_hashtag = $('#first-hashtag-input').val();
     var second_hashtag = $('#hash2').text();
     console.log(first_hashtag);
     console.log(second_hashtag);
 
-    var query = 'first_hashtag='+ first_hashtag +'&second_hashtag='+ second_hashtag
+    var query = 'first_hashtag='+ first_hashtag// +'&second_hashtag='//+ second_hashtag
 
     var socket = io.connect('https://twitter-burst-node.herokuapp.com/', { query: query, 'forceNew':true });
     socket.on('stream', function(tweet){
-        console.log(tweet);
+        
         setTimeout(function(){
-        if (tweet.search(first_hashtag) != -1){
             $('#first-hashtag-result').text(tweet);
             numone = $('#ticker-one').text();
             numone ++;
             $('#ticker-one').text(numone);
-        }}, 0);
-        setTimeout(function(){
-        if (tweet.search(second_hashtag) != -1) {
-            $('#second-hashtag-result').text(tweet);
-            numtwo = $('#ticker-two').text();
-            numtwo ++;
-            $('#ticker-two').text(numtwo);
-        }}, 0);
+        }, 0);
     });
     setTimeout(function () {
       socket.disconnect();
     }, 30000)
-});
+
+    var queryTwo = 'first_hashtag='+ second_hashtag
+
+    var socketTwo = io.connect('https://twitter-burst-node.herokuapp.com/', { query: queryTwo, 'forceNew':true });
+    socketTwo.on('stream', function(tweet){
+        console.log(tweet);
+        setTimeout(function(){
+        $('#second-hashtag-result').text(tweet);
+            numtwo = $('#ticker-two').text();
+            numtwo ++;
+            $('#ticker-two').text(numtwo);
+        }, 0)
+    });
+    setTimeout(function () {
+      socketTwo.disconnect();
+    }, 30000)
+}
+);
