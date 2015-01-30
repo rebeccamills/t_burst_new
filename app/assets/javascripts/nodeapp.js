2392f6609ad0eb841c0d17f030f7cf2860345e22
Emily C. McAfee <emilymcafee1@gmail.com>
2015-01-29 22:24:05 -0500

    working

--- app/assets/javascripts/nodeapp.js
+++ app/assets/javascripts/nodeapp.js
@@ -22,15 +22,19 @@ $('#start-button').on('click', function(){
     var socket = io.connect('https://twitter-burst-node.herokuapp.com/', { query: query, 'forceNew':true });
     socket.on('stream', function(tweet){
         setTimeout(function(){
-        if (tweet.search(second_hashtag) == -1){
+        if (tweet.search(first_hashtag) != -1){
             $('#first-hashtag-result').text(tweet);
             numone = $('#ticker-one').text();
             numone ++;
             $('#ticker-one').text(numone);
-        }
+        } else if (tweet.search(second_hashtag) != -1) {
+            $('#second-hashtag-result').text(tweet);
+            numtwo = $('#ticker-two').text();
+            numtwo ++;
+            $('#ticker-two').text(numtwo)}
         }, 0);
         setTimeout(function(){
-        if (tweet.search(first_hashtag) == -1) {
+        if (tweet.search(second_hashtag) != -1) {
             $('#second-hashtag-result').text(tweet);
             numtwo = $('#ticker-two').text();
             numtwo ++;
